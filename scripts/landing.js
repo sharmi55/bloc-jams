60657404afbbb8ebb2a081421f6fd06494150488
Sharmi <rai.sharmistha@gmail.com>
2016-07-10 20:11:38 +0200

    move selector to global scope

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,9 +1,8 @@
-var animatePoints = function () {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
-    var revealPoint = function(indexOfPoints) {
-        
+    var revealPoint = function(indexOfPoints) {       
         points[indexOfPoints].style.opacity = 1;
         points[indexOfPoints].style.transform = "scaleX(1) translateY(0)";
         points[indexOfPoints].style.msTransform = "scaleX(1) translateY(0)";
