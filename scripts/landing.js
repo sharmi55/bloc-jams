42315cdaf190e159311042355e08270575b5fa3c
Sharmi <rai.sharmistha@gmail.com>
2016-07-10 21:29:06 +0200

    set visible point of start of animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -15,8 +15,12 @@ var animatePoints = function(points) {
 
 window.onload = function() {
     var sellingPoints = document.getElementsByClassName('selling-points') [0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
     
     window.addEventListener('scroll', function(event) {
-        console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
     });
  }
