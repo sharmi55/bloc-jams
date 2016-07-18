6fba22ed3395c703ff18b3ad93fc3b24aa29298a
Sharmi <rai.sharmistha@gmail.com>
2016-07-18 16:18:38 +0200

    fix switch statement

--- scripts/album.js
+++ scripts/album.js
@@ -106,15 +106,16 @@ var findParentByClassName = function(element, targetClass){
 
 var getSongItem = function(element) {
     
-    switch(element.className) {
-        case 'album-song-button' || 'ion-play' || 'ion-pause':
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
             return findParentByClassName(element, 'song-item-number');
-       
         case 'album-view-song-item':
-            return element.querySelector('.song-item-number);
-        
-        case 'song-item-title' || 'song-item-duration':
-            return findParentByClassName(element, 'album-view-song-item');
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
         
         case 'song-item-number':
             return element;
