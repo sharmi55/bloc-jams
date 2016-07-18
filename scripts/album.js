1be22c6ddbdfded762043de871c0028686802be8
Sharmi <rai.sharmistha@gmail.com>
2016-07-18 14:06:00 +0200

    fix typo

--- scripts/album.js
+++ scripts/album.js
@@ -87,7 +87,7 @@ window.onload = function() {
 var findParentByClassName = function(element, targetClass){
     if (element) {
          var currentParent = element.parentElement;
-         while (currentParent.className != targetClass && currentParent.className != null) {
+         while (currentParent.className != targetClass && currentParent.className !== null) {
              currentParent = currentParent.parentElement;
          }
          return currentParent;
