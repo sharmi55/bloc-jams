cfa0f6186a7072cb1de26c371c4a1cdc3574131a
Sharmi <rai.sharmistha@gmail.com>
2016-07-19 16:07:54 +0200

    refactor animatePoints()

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,11 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-
-    var revealPoint = function(indexOfPoints) {       
-        points[indexOfPoints].style.opacity = 1;
-        points[indexOfPoints].style.transform = "scaleX(1) translateY(0)";
-        points[indexOfPoints].style.msTransform = "scaleX(1) translateY(0)";
-        points[indexOfPoints].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-        for(var i = 0; i < points.length; i++){
-            revealPoint(i);
-        }
+var animatePoints = function() {
+    var revealPoint = function() {       
+        $(this).css({
+            opacity: 1,
+            transfrom: 'scaleX(1) translateY(0)'
+        });
+    };
+    $.each($('.point'), revealPoint);
 };   
 
 $(window).load(function() {
