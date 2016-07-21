10d62832c5b54485904f8af8148394b077ed40cc
Sharmi <rai.sharmistha@gmail.com>
2016-07-21 20:30:55 +0200

    remove deprecated code

--- scripts/album.js
+++ scripts/album.js
@@ -85,9 +85,10 @@ var setCurrentAlbum = function(album) {
     }
 };
 
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
-var songRows = document.getElementsByClassName('album-view-song-item');
+var currentlyPlayingSong = null;
 
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
@@ -98,68 +99,8 @@ var currentlyPlayingSong = null;
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
-
-    for (var i = 0; i < songRows.length; i++) {
-        songRows[i].addEventListener('click', function(event) {
-                clickHandler(event.target);
-        });
-    }
 });   
 
 
-var findParentByClassName = function(element, targetClass){
-    if (element) {
-         var currentParent = element.parentElement;
-         while (currentParent.className != targetClass && currentParent.className !== null) {
-             currentParent = currentParent.parentElement;
-         }
-         return currentParent;
-     }
-       
-  };
-
-var getSongItem = function(element) {
-    
-    switch (element.className) {
-        case 'album-song-button':
-        case 'ion-play':
-        case 'ion-pause':
-            return findParentByClassName(element, 'song-item-number');
-        case 'album-view-song-item':
-            return element.querySelector('.song-item-number');
-        case 'song-item-title':
-        case 'song-item-duration':
-            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-        
-        case 'song-item-number':
-            return element;
-        
-        default:
-            return;
-    } 
-};
-
-
-var clickHandler = function(targetElement) {
-    
-    var songItem = getSongItem(targetElement);
-//when there's no currently playing song, do something?
-    if (currentlyPlayingSong === null) {
-        songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = songItem.getAttribute('data-song-number');
-    
-// when playing song is clicked again, revert button to play
-    } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-        songItem.innerHTML = playButtonTemplate;
-        currentlyPlayingSong = null;
- //when clicked song is not active son, set content of new song to pause button   
-    } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-        var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-        currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-        songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = songItem.getAttribute('data-song-number');
-        }  
-    
-};
 
         
