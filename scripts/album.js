bf272033ce09d5bbfd03d89d910f853ff3d337d4
Sharmi <rai.sharmistha@gmail.com>
2016-08-06 16:07:11 -0400

    enforce consistent data types

--- scripts/album.js
+++ scripts/album.js
@@ -10,7 +10,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 var $row = $(template);
 
 var clickHandler = function() {
-    var songNumber = $(this).attr('data-song-number');
+    var songNumber = parseInt($(this).attr('data-song-number'));
 
 	if (currentlyPlayingSongNumber !== null) {
 		// Revert to song number for currently playing song because user started playing new song.
@@ -36,7 +36,7 @@ var clickHandler = function() {
     
     var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = songNumberCell.attr('data-song-number');
+        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
         
         if(songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(playButtonTemplate);
@@ -45,7 +45,7 @@ var clickHandler = function() {
     
     var offHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = songNumberCell.attr('data-song-number');
+        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
 
         if(songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(songNumber);
